27d87ca43ddbf046c40b69caec31bf0fe0574893
AKASH RAJ PUROHITH <akashrajpurohit2006@gmail.com>
2026-07-08 21:58:02 +0530

    Time: 12 ms (36.26%), Space: 52.5 MB (85.64%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length/2;
+       Map<Integer, Integer> map = new HashMap();
+       for(int i : nums){
+        map.put(i, map.getOrDefault(i, 0) + 1);
+       }
+       for(int i:nums){
+        if(map.get(i) > n){
+            return i;
+        }
+       }
+       return -2;
+       } 
+    }
